deca1b45ab22bf22e40e1c66c22c05ae045a3d48
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-10-14 00:50:56 -0400

    Day 1 and Day 2 -> Complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,38 +2,77 @@
 
 import sys
 
+
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        #set the max size of ram
+        self.ram = [0] * 256
+        #create registers
+        self.register = [0] * 8
+    
+    #return the value stored in the address requested
+    def ram_read(self, address):
+        return self.ram[address]
+    
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        #address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        #program = [
+        #    # From print8.ls8
+        #    0b10000010, # LDI R0,8
+        #    0b00000000,
+        #    0b00001000,
+        #    0b01000111, # PRN R0
+        #    0b00000000,
+        #    0b00000001, # HLT
+        #]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        #for instruction in program:
+        #    self.ram[address] = instruction
+        #    address += 1
+
+        try:
+            address = 0
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    split_file = line.split("#")
+                    value = split_file[0].strip()
+                    if value == "":
+                        continue
+
+                    try:
+                        instruction = int(value, 2)
+                    
+                    except ValueError:
+                        print(f"The number is invalid: {instruction}")
+                        sys.exit(1)
+                    
+                    self.ram[address] = instruction
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]} {sys.argv[1]} -> File not found")
+            sys.exit()
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
+        
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
@@ -62,4 +101,40 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        self.running = True
+
+        while self.running:
+            #store the "instruction" in a variable
+            instruction = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if instruction == HLT: #HLT
+                self.running = False
+                self.pc +=1
+                #print("HLT: ", HLT)
+            
+            elif instruction == PRN:
+                #print(self.register[operand_a])
+                #print("Instruction: ", instruction)
+                #print("PRN: ", PRN)
+                self.pc += 2
+            
+            elif instruction == LDI:
+                self.register[operand_a] = operand_b
+                self.pc +=3                
+                #print("LDI: ", LDI)
+
+            elif instruction == MUL:
+                reg_a = self.ram_read(self.pc + 1)
+                reg_b = self.ram_read(self.pc + 2)
+                self.register[reg_a] = self.register[reg_a] * self.register[reg_b]
+                self.pc +=3
+                print("Register A: ", self.register[reg_a])
+                print("Program Counter: ", self.register)
+                
+
+            else:
+                print(f"unknown instructions {instruction} at address {pc}")
+                sys.exit(1)
+
